3f380f3bac65b36c5965cb10b28db5e88d0fe276
Thrank <silver.iava@gmail.com>
2021-04-07 17:39:44 +0200

    Tentativo di miglioramento cardview ristoratore

--- app/src/main/java/mobile/android/upf/ui/restaurant/restaurant_restaurants/RestaurantRestaurantsFragment.java
+++ app/src/main/java/mobile/android/upf/ui/restaurant/restaurant_restaurants/RestaurantRestaurantsFragment.java
@@ -2,6 +2,9 @@ package mobile.android.upf.ui.restaurant.restaurant_restaurants;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,16 +15,21 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,12 +39,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 import mobile.android.upf.AddRestaurantActivity;
 import mobile.android.upf.R;
 import mobile.android.upf.data.model.Order;
 import mobile.android.upf.data.model.RecyclerViewAdapter;
 import mobile.android.upf.data.model.RecyclerViewAdapter_restaurant;
 import mobile.android.upf.data.model.Restaurant;
+import mobile.android.upf.ui.client.client_homepage.ClientEditOrdersFragment;
 
 public class RestaurantRestaurantsFragment extends Fragment {
 
@@ -90,7 +100,7 @@ public class RestaurantRestaurantsFragment extends Fragment {
                     Log.d("firebase", String.valueOf(task.getResult().getValue()));
                     Iterable<DataSnapshot> restaurants_database = task.getResult().getChildren();
 
-                    for (DataSnapshot restaurant: restaurants_database) {
+                    for (DataSnapshot restaurant : restaurants_database) {
                         Log.d("firebase", String.valueOf(restaurant.child("name").getValue()));
                         if (String.valueOf(restaurant.child("restaurateur_id").getValue()).equals(userId)) {
                             lstRest.add(new Restaurant(String.valueOf(restaurant.child("name").getValue()),
@@ -100,7 +110,7 @@ public class RestaurantRestaurantsFragment extends Fragment {
                                     String.valueOf(restaurant.child("phone").getValue()),
                                     String.valueOf(restaurant.child("restaurateur_id").getValue()),
                                     String.valueOf(restaurant.child("imageUrl").getValue()),
-                                    0));
+                                    Integer.valueOf((Integer) restaurant.child("status").getValue())));
                         }
 
                     }
@@ -109,15 +119,91 @@ public class RestaurantRestaurantsFragment extends Fragment {
                     myrv = (RecyclerView) root.findViewById(R.id.recyclerview_restaurant_restaurants);
                     myAdapter = new RecyclerViewAdapter_restaurant(getActivity(), lstRest);
 
-                    myrv.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+                    myrv.setLayoutManager(new GridLayoutManager(getActivity(), 1));
                     myrv.setAdapter(myAdapter);
 
+                    ItemTouchHelper itemTouchHelperSwipe = new ItemTouchHelper(simpleCallbackSwipe);
+                    itemTouchHelperSwipe.attachToRecyclerView(myrv);
                 }
 
 
             }
+
+            Restaurant deletedItem = null;
+            //Gestisco gli swipe a destra e sinistra
+            ItemTouchHelper.SimpleCallback simpleCallbackSwipe = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                @Override
+                public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                    return false;
+                }
+
+                @Override
+                public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+                    int pos = viewHolder.getAdapterPosition();
+
+                    switch (direction) {
+                        case ItemTouchHelper.LEFT: //cancello l'elemento
+                            deletedItem = lstRest.get(pos);
+                            lstRest.remove(pos);
+                            myAdapter.notifyItemRemoved(pos);
+                            Snackbar.make(myrv, deletedItem.toString(), Snackbar.LENGTH_LONG).setAction(getString(R.string.undo), new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    lstRest.add(pos, deletedItem);
+                                    myAdapter.notifyItemInserted(pos);
+                                }
+                            }).show();
+
+                            break;
+                        case ItemTouchHelper.RIGHT: //modifico l'elemento
+                            ClientEditOrdersFragment dialogEditOrderFragment = new ClientEditOrdersFragment();
+                            dialogEditOrderFragment.show(getChildFragmentManager(), "EditFragment");
+
+                            deletedItem = lstRest.get(pos);
+                            lstRest.remove(pos);
+                            myAdapter.notifyItemRemoved(pos);
+                            //sarà necessario fare un aggiornamento dei dati
+                            lstRest.add(pos, deletedItem);
+                            myAdapter.notifyItemInserted(pos);
+                            break;
+                    }
+
+                }
+
+                @Override
+                public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                    new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                            .addSwipeLeftBackgroundColor(ContextCompat.getColor(getActivity(), R.color.design_default_color_error))
+                            .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_24)
+                            .addSwipeRightBackgroundColor(ContextCompat.getColor(getActivity(), R.color.design_default_color_secondary))
+                            .addSwipeRightActionIcon(R.drawable.ic_baseline_edit_24)
+                            .create()
+                            .decorate();
+
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                }
+            };
+
         });
 
         return root;
     }
+
+    /*public void updateImageView(String restaurantId) {
+        mDatabase.child("Users").child(restaurantId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting image data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+
+                    Uri uri = Uri.parse(String.valueOf(task.getResult().child("imageUrl").getValue()));
+                    Log.d("firebase", "Image Url: " + uri);
+                    Glide.with(applicationContext).load(uri).into(restaurant_pic);
+                }
+            }
+        });
+    }*/
 }
