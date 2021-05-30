3d21d5c765454d5c70b1786acd5edfb47b773765
Thrank <silver.iava@gmail.com>
2021-05-30 15:32:05 +0200

    Bozza 4 statistiche

--- app/src/main/java/mobile/android/upf/ui/restaurant/restaurant_analytics/RestaurantAnalyticsFragment.java
+++ app/src/main/java/mobile/android/upf/ui/restaurant/restaurant_analytics/RestaurantAnalyticsFragment.java
@@ -49,9 +49,10 @@ public class RestaurantAnalyticsFragment extends Fragment {
     private ArrayList<Dish> lstDishes;
     private ArrayList<BarEntry> lstEntries;
     private ArrayList<String> lstLabels;
-    private ArrayList<BarDataSet> lstBarDataSet;
+    private ArrayList<Integer> lstColors;
     private BarDataSet barDataSet;
     private BarChart barChart;
+    private BarData barData;
     private String userId;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -75,7 +76,8 @@ public class RestaurantAnalyticsFragment extends Fragment {
         lstDishes = new ArrayList<>();
         lstEntries = new ArrayList<>();
         lstLabels = new ArrayList<>();
-        lstBarDataSet = new ArrayList<>();
+        lstColors = new ArrayList<>();
+        barData = new BarData();
 
         mDatabase.child("Restaurants").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
@@ -85,10 +87,23 @@ public class RestaurantAnalyticsFragment extends Fragment {
                 } else {
                     Log.d("firebase", String.valueOf(task.getResult().getValue()));
                     Iterable<DataSnapshot> restaurants_database = task.getResult().getChildren();
-
+//                    int counter = 0;
+//                    for (DataSnapshot restaurant : restaurants_database) {
+//                        if (restaurant.child("restaurateur_id").getValue().equals(userId)) {
+//                            counter++;
+//                        }
+//                    }
+//                    for (int i = 0; i < counter; i++) {
+//                        Random rnd = new Random();
+//                        int color = Color.rgb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+//                        lstColors.add(color);
+//                    }
+//                    int colorCount = 0;
                     for (DataSnapshot restaurant : restaurants_database) {
                         if (String.valueOf(restaurant.child("restaurateur_id").getValue()).equals(userId)) {
+                            Log.d("RISTORANTE", String.valueOf(restaurant.child("name").getValue()));
                             lstDishes.clear();
+//                            int finalColorCount = colorCount;
                             mDatabase.child("Dishes").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
                                 @Override
                                 public void onComplete(@NonNull @NotNull Task<DataSnapshot> task) {
@@ -100,6 +115,7 @@ public class RestaurantAnalyticsFragment extends Fragment {
                                         for (DataSnapshot dish : dishes_database) {
                                             if (String.valueOf(dish.child("restaurant_id").getValue()).equals(
                                                     String.valueOf(restaurant.child("id").getValue()))) {
+                                                Log.d("PIATTI", String.valueOf(dish.child("name").getValue()));
                                                 lstDishes.add(new Dish(
                                                         String.valueOf(dish.getKey()),
                                                         String.valueOf(dish.child("name").getValue()),
@@ -109,45 +125,48 @@ public class RestaurantAnalyticsFragment extends Fragment {
                                                         Integer.parseInt(String.valueOf(dish.child("number").getValue()))));
                                             }
                                         }
+                                        lstEntries.clear();
+                                        lstLabels.clear();
+                                        int counter = 0;
+                                        Log.d("DISH", String.valueOf(lstDishes.size()));
+                                        for (Dish dish : lstDishes) {
+                                            Log.d("DISH", dish.getName());
+                                            lstEntries.add(new BarEntry(counter, dish.getNumber()));
+                                            lstLabels.add(dish.getName());
+                                            counter++;
+                                        }
+                                        Log.d("ENTRIES", String.valueOf(lstEntries.size()));
+                                        if (lstEntries.size() != 0) {
+                                            barDataSet = new BarDataSet(lstEntries, String.valueOf(restaurant.child("name").getValue()));
+//                                            Log.d("COLOR", String.valueOf(lstColors.get(finalColorCount)));
+//                                            barDataSet.setColors(lstColors.get(finalColorCount));
+                                            Random rnd = new Random();
+                                            int color = Color.rgb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                                            barDataSet.setColor(color);
+                                            barDataSet.setValueTextColor(Color.DKGRAY);
+                                            barDataSet.setValueTextSize(16f);
+                                            barData.addDataSet(barDataSet);
+                                        }
+
+                                        barChart.setFitBars(true);
+                                        barChart.setData(barData);
+                                        barChart.getDescription().setText("Istogramma");
+                                        XAxis xAxis = barChart.getXAxis();
+                                        //xAxis.setLabelCount(lstEntries.size(), true);
+                                        xAxis.setValueFormatter(new IndexAxisValueFormatter(lstLabels));
+                                        xAxis.setLabelRotationAngle(-90);
+                                        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                                        barChart.animateY(2000);
+//                                        for (BarDataSet set : lstBarDataSet) {
+//                                            barData.addDataSet(set);
+//                                        }
                                     }
                                 }
                             });
-                            lstEntries.clear();
-                            lstLabels.clear();
-                            int counter = 0;
-                            for (Dish dish : lstDishes) {
-                                Log.d("Dish", dish.getName());
-                                if (restaurant.child("id").getValue().equals(dish.getRestaurant_id())) {
-                                    lstEntries.add(new BarEntry(counter, dish.getNumber()));
-                                    lstLabels.add(dish.getName());
-                                    counter++;
-                                }
-                            }
-                            if (lstEntries.size() != 0) {
-                                barDataSet = new BarDataSet(lstEntries, String.valueOf(restaurant.child("name").getValue()));
-                                Random rnd = new Random();
-                                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-                                barDataSet.setColor(color);
-                                barDataSet.setValueTextColor(Color.DKGRAY);
-                                barDataSet.setValueTextSize(16f);
-                                lstBarDataSet.add(barDataSet);
-                            }
                         }
+//                        colorCount++;
                     }
                     // Fine ciclo for ristoranti
-                    Log.d("DATI", "CIAO");
-                    BarData barData = new BarData(lstBarDataSet);
-                    Log.d("DATI", "CIAO2");
-                    barChart.setFitBars(true);
-                    barChart.setData(barData);
-                    barChart.getDescription().setText("Istogramma");
-                    XAxis xAxis = barChart.getXAxis();
-                    //xAxis.setLabelCount(lstEntries.size(), true);
-                    xAxis.setValueFormatter(new IndexAxisValueFormatter(lstLabels));
-                    xAxis.setLabelRotationAngle(-45);
-                    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-                    barChart.animateY(2000);
-                    Log.d("DATI", "CIAO3");
                 }
             }
         });
